511b83b559de20013ec9d30076e2777cdd4e7423
Pasan Kavinga <pasan@incubatelabs.com>
2021-08-11 11:52:30 +0530

    indentation issue fix attempt two

--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -32,7 +32,8 @@ class ConfirmablePasswordController extends Controller
             ! Auth::guard('web')->validate([
             'email' => $request->user()->email,
             'password' => $request->password,
-            ])) {
+            ])
+           ) {
             throw ValidationException::withMessages([
                 'password' => __('auth.password'),
             ]);
